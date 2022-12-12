548dff35f2ab63fa378b4af96b83334419e1c148
KenyerLord89 <kovimaci03@gmail.com>
2022-12-12 14:54:48 +0100

    v1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while(!false) {
+            Scanner valami = new Scanner(System.in);
+            String valami2 = valami.nextLine();
+
+            if (Objects.equals(valami2, "hello"))
+            {
+                System.out.println("Hello World");
+            }
+            else if (Objects.equals(valami2, "info"))
+            {
+                System.out.println("v1");
+            }
+            else if (Objects.equals(valami2, "exit"))
+            {
+                System.out.println("Adiós");
+            }
+        }
     }
 }
